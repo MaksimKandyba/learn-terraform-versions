b1785354bb0fcae78f7b62aca401079550a60b03
kandyba <m.kandyba@comparus.de>
2023-05-10 18:28:20 +0200

    draft

--- main.tf
+++ main.tf
@@ -12,3 +12,58 @@ terraform {
 
   required_version = "~> 1.4.6"
 }
+
+provider "fly" {
+  fly_http_endpoint = "api.machines.dev"
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {}
+
+resource "fly_app" "exampleApp" {
+  name = "kandyba-flyiac"
+  org  = "personal"
+}
+
+resource "fly_ip" "exampleIp" {
+  app        = "kandyba-flyiac"
+  type       = "v4"
+  depends_on = [fly_app.exampleApp]
+}
+
+resource "fly_ip" "exampleIpv6" {
+  app        = "kandyba-flyiac"
+  type       = "v6"
+  depends_on = [fly_app.exampleApp]
+}
+
+resource "fly_machine" "web" {
+  app    = "kandyba-flyiac"
+  region = "ams"
+  name   = random_pet.name.id
+  image  = "amazonlinux:latest"
+  services = [
+    {
+      ports = [
+        {
+          port     = 443
+          handlers = ["tls", "http"]
+        },
+        {
+          port     = 80
+          handlers = ["http"]
+        }
+      ]
+      "protocol" : "tcp",
+      "internal_port" : 80
+    },
+  ]
+  cpus       = 1
+  memorymb   = 256
+  depends_on = [fly_app.exampleApp]
+  cmd = [
+    "yum -y remove httpd",
+    "yum -y remove httpd-tools"
+  ]
+}
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "cpus" {
+  value = fly_machine.web.cpus
+}
+
+output "memorymb" {
+  value = fly_machine.web.memorymb
+}
